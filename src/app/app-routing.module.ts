d131b6dddef99fdbb5b8062d74480ac447b35c5d
AchiraDeSilva <achiradesilva123@gmail.com>
2021-02-02 07:22:40 +0500

    init-01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResultComponent } from './components/result/result.component';
+import {DashComponent} from './components/dash/dash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path :  'register' ,  component : RegisterComponent},
+  { path :  'result' ,  component : ResultComponent},
+  { path :  'quiz' ,  component : QuizComponent},
+  { path :  'dash' ,  component : DashComponent},
+  { path :  '' ,  redirectTo : '/register' , pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { ResultComponent } from './components/result/result.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { DashComponent } from './components/dash/dash.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
